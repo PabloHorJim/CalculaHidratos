a1bee858acbab931e29dabb709d3dad3db032f8b
PabloHorJim <pablohorjim@uma.es>
2026-03-29 19:35:43 +0200

    Fix: Remove maskable purpose to stop Android icon zooming

--- vite.config.ts
+++ vite.config.ts
@@ -27,20 +27,12 @@ export default defineConfig(({mode}) => {
             {
               src: 'logo.png',
               sizes: '192x192',
-              type: 'image/png',
-              purpose: 'any'
+              type: 'image/png'
             },
             {
               src: 'logo.png',
               sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any'
-            },
-            {
-              src: 'logo.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'maskable'
+              type: 'image/png'
             }
           ]
         },
